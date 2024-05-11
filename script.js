1f5d31bf18345a4652d11a22ff59784c6984d4be
Rachit Srivastava <honey032004@gmail.com>
2024-05-12 01:00:14 +0530

    keyboardsupp

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 const display = document.getElementById('display');
 
+
 function appendToDisplay(input){
+    
     display.value += input;
 
 
@@ -23,3 +25,59 @@ function Calculate(){
     
 }
 
+
+document.onkeyup =  e => {
+    if(e.key == "+"){
+        display.value += "+";
+    }
+    else if(e.key == "7"){
+        display.value += "7";
+    }
+    else if(e.key == "8"){
+        display.value += "8";
+    }
+    else if(e.key == "9"){
+        display.value += "9";
+    }
+    else if(e.key == "-"){
+        display.value += "-";
+    }
+    else if(e.key == "4"){
+        display.value  += "4";
+    }
+    else if(e.key == "5"){
+        display.value += "5";
+    }
+    else if(e.key == "6"){
+        display.value += "6";
+    }
+    else if(e.key == "*"){
+        display.value += "*";
+    }
+    else if(e.key == "1"){
+        display.value += "1";
+    }
+    else if(e.key == "2"){
+        display.value  += "2";
+    }
+    else if(e.key == "3"){
+        display.value  += "3";
+    }
+    else if(e.key == "/"){
+        display.value += "/";
+    }
+    else if(e.key == "0"){
+        display.value  += "0";
+    }
+    else if(e.key == "."){
+        display.value  += ".";
+    }
+    else if(e.key == "="){
+        display.value = eval(display.value);
+    }
+    else if(e.key == "C"){
+        display.value = "";
+    }
+
+
+}
